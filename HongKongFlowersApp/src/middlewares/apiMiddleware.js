ce709104f2a5dac0ea28a860ac1c3013124ed49c
Jack Lam <jacklam718@gmail.com>
2017-09-07 18:42:24 +0800

    Make apiMiddleware more functional programming style

--- HongKongFlowersApp/src/middlewares/apiMiddleware.js
+++ HongKongFlowersApp/src/middlewares/apiMiddleware.js
@@ -3,18 +3,24 @@ import Config from 'react-native-config';
 import { apiSuccess, apiError } from '../actions';
 import { API_REQUEST, API_POST, API_UPLOAD } from '../constants';
 
-const BASE_API_URL = Config.API_URL;
+const API_URL = Config.API_URL;
 
-const isApiAction = type => [API_REQUEST, API_POST, API_UPLOAD].includes(type);
-
-const createHttpOptions = ({ url, method, data, headers }) => {
+/*
+  Example:
+  const v1API = requester('http://example.com/v1');
+  const v2API = requester('http://example.com/v2');
+  v1API({ url, type, data, headers });
+*/
+export const requester = apiUrl => async ({ url, method, data, headers }) => {
   const defaultHeaders = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   };
 
-  return {
-    url,
+  const newApiUrl = apiUrl.endsWith('/') ? apiUrl.slice(0, -1) : apiUrl;
+  const newUrl = url.startsWith('/') ? url.slice(1) : url;
+  const options = {
+    url: `${newApiUrl}/${newUrl}`,
     method,
     data,
     headers: {
@@ -22,33 +28,10 @@ const createHttpOptions = ({ url, method, data, headers }) => {
       ...headers,
     },
   };
-};
 
-const api = async ({ url, data, type, headers }) => {
-  // select a http method;
-  let method;
-  switch (type) {
-    case API_POST:
-    case API_UPLOAD:
-      method = 'POST';
-      break;
-    case API_REQUEST:
-    default:
-      method = 'GET';
-      break;
-  }
-
-  // handle all api response
+  // handle api response
   try {
-    const response = await axios(
-      createHttpOptions({
-        url: `${BASE_API_URL}${url}`,
-        method,
-        data,
-        headers,
-      }),
-    );
-    return response.data;
+    return await (await axios(options)).data;
   } catch (error) {
     throw error;
   }
@@ -57,42 +40,52 @@ const api = async ({ url, data, type, headers }) => {
 /*
   A apiMiddleware support action callbacks and centralized all api success, error handling strategy
 */
-export default ({ dispatch }) => next => (action) => {
-  const handleSuccess = (data) => {
-    dispatch(apiSuccess({
-      type: action.payload.next.SUCCESS,
-      data,
-    }));
-    return data;
-  };
+export const apiMiddlewareFactory = api => ({ dispatch }) => (next) => {
+  const isApiAction = type => [API_REQUEST, API_POST, API_UPLOAD].includes(type);
 
-  const notify = (data) => {
-    if (action.payload.success) {
-      action.payload.success.forEach((callback) => {
-        dispatch(callback(data));
-      });
+  return (action) => {
+    const handleSuccess = (data) => {
+      dispatch(apiSuccess({ type: action.payload.next.SUCCESS, data }));
+      return data;
+    };
+
+    const notify = (data) => {
+      (action.payload.success || [])
+        .forEach(callback => dispatch(callback(data)));
+      return data;
+    };
+
+    const handleError = (error) => {
+      dispatch(apiError({ type: action.payload.next.ERROR, error }));
+      return error;
+    };
+
+    // handle all api calls
+    if (isApiAction(action.type)) {
+      dispatch({ type: action.payload.next.PENDING });
+
+      // select a http method
+      let method;
+      switch (action.type) {
+        case API_POST:
+        case API_UPLOAD:
+          method = 'POST';
+          break;
+        case API_REQUEST:
+        default:
+          method = 'GET';
+          break;
+      }
+
+      // api chaining
+      api({ ...action.payload, method })
+        .then(handleSuccess)
+        .then(notify)
+        .catch(handleError);
     }
-    return data;
+
+    return next(action);
   };
-
-  const handleError = (error) => {
-    dispatch(apiError({
-      type: action.payload.next.ERROR,
-      error,
-    }));
-    return error;
-  };
-
-  // handle all api calls
-  if (isApiAction(action.type)) {
-    dispatch({ type: action.payload.next.PENDING });
-
-    // api chaining
-    api({ ...action.payload, type: action.type })
-      .then(handleSuccess)
-      .then(notify)
-      .catch(handleError);
-  }
-
-  return next(action);
 };
+
+export default apiMiddlewareFactory(requester(API_URL));
