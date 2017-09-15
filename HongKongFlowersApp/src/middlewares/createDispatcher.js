112e3e446b9b0214c0577cfd6c460cd2634eee8e
Jack Lam <jacklam718@gmail.com>
2017-09-16 00:53:46 +0800

    Add type checking on dispatchers

--- HongKongFlowersApp/src/middlewares/createDispatcher.js
+++ HongKongFlowersApp/src/middlewares/createDispatcher.js
@@ -1,22 +1,24 @@
+// @flow
+
 import { apiSuccess, apiError } from '../actions';
 
-export default (dispatch, action) => ({
-  success: (data) => {
+export default (dispatch: Function, action: Object) => ({
+  success(data?: any): any {
     dispatch(apiSuccess({ type: action.payload.next.SUCCESS, data }));
     return data;
   },
 
-  notify: (data) => {
+  notify(data?: any): any {
     (action.payload.success || [])
       .forEach(callback => dispatch(callback(data)));
     return data;
   },
 
-  start: () => {
+  start(): void {
     dispatch({ type: action.payload.next.PENDING });
   },
 
-  error: (error) => {
+  error(error?: Object): Object {
     dispatch(apiError({ type: action.payload.next.ERROR, error }));
     return error;
   },
