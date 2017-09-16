cc3c3a2faa985bb4a1114897372de12296a091a7
Jack Lam <jacklam718@gmail.com>
2017-09-16 16:33:06 +0800

    Add Geolocation component

---
+++ HongKongFlowersApp/src/common/Geolocation.js
@@ -0,0 +1,67 @@
+// @flow
+
+import { PureComponent } from 'react';
+
+type Props = {
+  onPositionUpdated?: () => void,
+  onError?: () => void,
+}
+
+export default class Geolocation extends PureComponent {
+  static defaultProps = {
+    onPositionUpdated: () => {},
+    onError: () => {},
+  }
+
+  static options = {
+    enableHighAccuracy: true,
+    timeout: 20000,
+    maximumAge: 1000,
+    distanceFilter: 10,
+  };
+
+  state = {
+    latitude: null,
+    longitude: null,
+    error: null,
+  };
+
+  componentDidMount() {
+    this.watchId = navigator.geolocation.watchPosition(
+      this.handlePositionUpdate,
+      this.handleError,
+      Geolocation.options,
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  getPosition = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+  })
+
+  props: Props
+
+  handlePositionUpdate = (position) => {
+    const { onPositionUpdated } = this.props;
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      error: null,
+    });
+    onPositionUpdated(position);
+  }
+
+  handleError = (error) => {
+    const { onError } = this.props;
+    this.setState({ error: error.message });
+    onError(error);
+  }
+
+  render() {
+    return null;
+  }
+}
--- HongKongFlowersApp/src/common/index.js
+++ HongKongFlowersApp/src/common/index.js
@@ -2,3 +2,4 @@ export Camera from './Camera';
 export FBLoginButton from './FBLoginButton';
 export Spinner from './Spinner';
 export Snackbar from './Snackbar';
+export Geolocation from './Geolocation';
