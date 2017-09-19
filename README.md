f5d5bf3d05120fcb8cb257b9f1c9221d76bf6453
Jack Lam <jacklam718@gmail.com>
2017-09-19 22:47:09 +0800

    Only keep mobile on this repo

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Hong Kong Flower Identitfication APP
-An APP for identify flowers in Hong Kong with Deep Learning technology.
+## Hong Kong Flower Recognition App
+
+## Tech Stack
+* React Native
+* ES6
+* Yarn (Package Manager)
+* Jest (Testing Framework)
+* Flow (Static Type Checker for JavaScript)
+* Redux
+* Firebase
+
+## Development
+### INSTALL
+### Install node modules
+```
+yarn global react-native-cli
+yarn
+```
+
+### If you are running this project for the first time
+Please run `react-native link` to link native modules:
+```
+react-native link
+```
+
+### RUN iOS
+```
+react-native run-ios
+```
+
+### RUN Android
+```
+react-native run-android
+```
+
+### RUN Mock RESTFUL API Server
+```
+yarn run start:mock-rest-server
+```
+
+## Code Style Guide
+* **[Airbnb JS Style Guide](https://github.com/airbnb/javascript)**
+* **2 spaces indentation**
---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components';
+import { Provider } from 'react-redux';
+import store from './reducers';
+import LoginModal from './login/LoginModal';
+import CameraView from './views/CameraView';
+import { ParallaxPager } from './common';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  navigator: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+class App extends Component {
+  // eslint-disable-next-line
+  configureScene(route) {
+    if (route.name === 'login') {
+      return Navigator.SceneConfigs.FloatFromBottom;
+    }
+    if (route.name === 'camera') {
+      return Navigator.SceneConfigs.HorizontalSwipeJump;
+      // return null;
+    }
+    return Navigator.SceneConfigs.FloatFromRight;
+  }
+
+  // eslint-disable-next-line
+  renderScene(route, navigator) {
+    if (route.name === 'login') {
+      return <LoginModal navigator={navigator} />;
+    }
+    if (route.name === 'camera') {
+      return <CameraView navigator={navigator} />;
+    }
+    return <ParallaxPager navigator={navigator} />;
+    // return <CameraView navigator={navigator} />;
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Navigator
+          initialRoute={{
+            name: '',
+            title: '',
+          }}
+          renderScene={this.renderScene}
+          configureScene={this.configureScene}
+          style={styles.navigator}
+        />
+      </Provider>
+    );
+  }
+}
+
+// eslint-disable-next-line
+console.ignoredYellowBox = ['Remote debugger'];
+export default App;
