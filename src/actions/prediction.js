229f41024293107eed584e10d4e458a12abc37ea
Jack Lam <jacklam718@gmail.com>
2017-10-20 03:02:40 +0800

    Set uid on precaution action

--- src/actions/prediction.js
+++ src/actions/prediction.js
@@ -1,15 +1,18 @@
 // @flow
 
+import store from '../store';
 import { API_UPLOAD, FLOWER_PREDICTION } from '../constants';
 
 // eslint-disable-next-line
 export function fetchFlowerPrediction({ image, lat, lng }) {
+  const { userId: uid } = store.getStore().getState().auth;
   return {
     type: API_UPLOAD,
     payload: {
       endpoint: '/predict',
       data: {
         image,
+        uid,
         lat,
         lng,
       },
