719494419fac49363bc6ea5985643c1faa8b6239
Jack Lam <jacklam718@gmail.com>
2017-10-20 03:01:20 +0800

    Refactor store

--- src/setup.js
+++ src/setup.js
@@ -1,12 +1,12 @@
 import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
-import createStore from './store/createStore';
+import store from './store';
 import App from './App';
 
 export default function setup() {
   return class Root extends PureComponent {
     state = {
-      store: createStore(() => this.setState({ loaded: true })),
+      store: store.createStore(() => this.setState({ loaded: true })),
       loaded: false,
     }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,44 @@
-export createStore from './createStore';
+import { AsyncStorage } from 'react-native';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+// eslint-disable-next-line
+import { createLogger } from 'redux-logger';
+import { autoRehydrate, persistStore } from 'redux-persist';
+import { apiMiddleware, firebaseMiddleware } from '../middlewares';
+import reducers from '../reducers';
+
+const middlewares = [
+  apiMiddleware,
+  firebaseMiddleware,
+];
+
+if (__DEV__ === true) {
+  middlewares.push(createLogger({
+    duration: true,
+    timestamp: true,
+    diff: true,
+  }));
+}
+
+const appStore = {
+  setStore(store) {
+    appStore.store = store;
+  },
+  getStore() {
+    return appStore.store;
+  },
+  createStore(onComplete: ?() => void) {
+    const store = createStore(
+      combineReducers(reducers),
+      compose(
+        applyMiddleware(...middlewares),
+        autoRehydrate(),
+      ),
+    );
+
+    persistStore(store, { storage: AsyncStorage }, onComplete);
+    appStore.setStore(store);
+    return store;
+  },
+};
+
+export default appStore;
